46322765540465b53485566bbcfb3e4515d1ebdd
Vladimir Baev <extless@gmail.com>
2022-08-14 11:56:23 +0700

    Extract HTTP calls to `MakeRequest`

--- app/src/main/java/com/vetclinic/app/common/network/HttpService.kt
+++ app/src/main/java/com/vetclinic/app/common/network/HttpService.kt
@@ -1,34 +1,7 @@
 package com.vetclinic.app.common.network
 
-import com.vetclinic.app.common.network.exception.BadHttpServiceCall
-import okhttp3.*
-import java.io.IOException
+import okhttp3.OkHttpClient
 
-abstract class HttpService<R>(private val client: OkHttpClient) {
-
-    abstract fun get(
-        onResult: (R) -> Unit,
-        onError: (Throwable) -> Unit
-    )
-
-    protected fun makeRequest(
-        url: String,
-        onResult: (Response) -> Unit,
-        onError: (Throwable) -> Unit
-    ) {
-        val request = Request.Builder().url(url).build()
-        client.newCall(request).enqueue(object : Callback {
-            override fun onFailure(call: Call, e: IOException) {
-                onError.invoke(e)
-            }
-
-            override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    onResult.invoke(response)
-                } else {
-                    onError.invoke(BadHttpServiceCall())
-                }
-            }
-        })
-    }
+abstract class HttpService<R>(client: OkHttpClient) : MakeRequest(client) {
+    abstract fun get(onResult: (R) -> Unit, onError: (Throwable) -> Unit)
 }
---
+++ app/src/main/java/com/vetclinic/app/common/network/MakeRequest.kt
@@ -0,0 +1,28 @@
+package com.vetclinic.app.common.network
+
+import com.vetclinic.app.common.network.exception.BadHttpServiceCall
+import okhttp3.*
+import java.io.IOException
+
+abstract class MakeRequest(private val client: OkHttpClient) {
+    fun makeRequest(
+        url: String,
+        onResult: (Response) -> Unit,
+        onError: (Throwable) -> Unit
+    ) {
+        val request = Request.Builder().url(url).build()
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                onError.invoke(e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                if (response.isSuccessful) {
+                    onResult.invoke(response)
+                } else {
+                    onError.invoke(BadHttpServiceCall())
+                }
+            }
+        })
+    }
+}
